29d33243fcf7717be5f5cf01696e1f266a7132c7
Wardayaz <wayaz107@gmail.com>
2021-02-17 09:03:02 -0600

    created adapter method

--- src/adapters/BooksAdapter.js
+++ src/adapters/BooksAdapter.js
@@ -2,4 +2,9 @@ class BooksAdapter{
     constructor(){
         this.baseUrl = 'http://localhost:3000/books'
     }
+
+    getBooks(){
+        return fetch(this.baseUrl)
+        .then(res => res.json())
+    }
 }

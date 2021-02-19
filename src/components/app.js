f9da48b011421557c709771692eef651214281fa
Wardayaz <wayaz107@gmail.com>
2021-02-19 01:57:46 -0600

    update file structure

--- src/components/app.js
+++
@@ -1,5 +0,0 @@
-class App{
-    constructor(){
-        this.books = new Books()
-    }
-}
--- src/components/books.js
+++
@@ -1,6 +0,0 @@
-class Books{
-    constructor(){
-        this.books = []
-        this.adapter = new BooksAdapter()
-    }
-}

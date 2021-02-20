548e47a327115951fdcd9869881ea1d7bf97c9f6
Wardayaz <wayaz107@gmail.com>
2021-02-20 01:23:29 -0600

    created getPerfumes function

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ const BACKEND_URL = 'http://localhost:3000/api/v1';
 const ALLPERFUMES_URL = `${BACKEND_URL}/perfumes`;
 const ALLREVIEWS_URL = `${BACKEND_URL}/reviews`;
 
-document.addEventListener('DOMContentLoaded', function(e){
-    
-})
+// document.addEventListener('DOMContentLoaded', function(e){
 
-fetch('http://localhost:3000/api/v1/perfumes')
-  .then(response => response.json())
-  .then(data => console.log(data));
+// })
+
+// load perfumes 
+function getPerfumes(){
+    fetch(ALLPERFUMES_URL)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+getPerfumes()

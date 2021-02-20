cda34772c55fa19df3acba5d1a16a28117b5602e
Wardayaz <wayaz107@gmail.com>
2021-02-20 01:09:49 -0600

    updated perfume class

--- src/components/perfume.js
+++ src/components/perfume.js
@@ -1,3 +1,13 @@
 class Perfume{
+   static allPerfume = [];
 
+   constructor({id, name, brand, image, reviews}){
+       this.id = id
+       this.name = name
+       this.brand = brand
+       this.image = image
+       this.reviews = reviews
+
+       Perfume.allPerfume.push(this)
+   }
 }

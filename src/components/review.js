1fa4ee45f0cf0cc792b00501490539acc270af9b
Wardayaz <wayaz107@gmail.com>
2021-02-20 01:12:56 -0600

    updated review class

--- src/components/review.js
+++ src/components/review.js
@@ -1,2 +1,11 @@
 class Review{
+    static allReviews = [];
+
+    constructor({id, perfume_id, body}){
+      this.id = id
+      this.perfume = perfume_id
+      this.body = body
+
+      Review.allReviews.push(this)
+    }
 }

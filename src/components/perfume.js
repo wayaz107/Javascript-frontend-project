52dfa84fb7c1661059cf313f9828a480f71ea75a
Wardayaz <wayaz107@gmail.com>
2021-02-20 03:30:54 -0600

    updated renderPerfume method

--- src/components/perfume.js
+++ src/components/perfume.js
@@ -1,12 +1,12 @@
 class Perfume{
    static allPerfume = [];
 
-   constructor({id, name, brand}){
+   constructor({id, name, brand, reviews = []}){
        this.id = id
        this.name = name
        this.brand = brand
     //    this.image = image
-    //    this.reviews = reviews
+       this.reviews = reviews
 
        Perfume.allPerfume.push(this)
    }
@@ -17,15 +17,22 @@ class Perfume{
     // create div
        const perfumeDiv = document.createElement('div');
 
-    // manipulate div
-       perfumeDiv.innnerHTML = `
-         <h3> class="card-title"> Name: ${this.name}</h3>
-         <h5> class="card-content"> Brand: ${this.brand}</h5> 
-       `
+    //render review
+    if (this.reviews !=0){
+        this.renderPerfumeReviews()
+    }
+    else{
+      // manipulate div
+      perfumeDiv.innnerHTML = `
+      <h3> class="card-title"> Name: ${this.name}</h3>
+      <h5> class="card-content"> Brand: ${this.brand}</h5> 
+    `
+      // insert div 
+      perfumeDiv.setAttribute('data.id', `${this.id}`)
+      perfumeDiv.className = 'card col s6'
+      perfumes.appendChild(perfumeDiv)
 
-    // insert div 
-    perfumeDiv.setAttribute('data.id', `${this.id}`)
-    perfumeDiv.className = 'card col s6'
-    perfumes.appendChild(perfumeDiv)
+    }
+  
    }
 }

be0fa03207109a9339f0f6df6d9b616ce2a073a0
Wardayaz <wayaz107@gmail.com>
2021-02-20 01:16:41 -0600

    added domcontentloaded

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const BACKEND_URL = 'http://localhost:3000/api/v1';
 const ALLPERFUMES_URL = `${BACKEND_URL}/perfumes`;
 const ALLREVIEWS_URL = `${BACKEND_URL}/reviews`;
 
-
+document.addEventListener('DOMContentLoaded', function(e){
+    
+})
 
 fetch('http://localhost:3000/api/v1/perfumes')
   .then(response => response.json())

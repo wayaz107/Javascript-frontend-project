ddb26d1756f0d0be9bdc86ffdcdc43e76a1fadab
Wardayaz <wayaz107@gmail.com>
2021-02-20 02:04:25 -0600

    added innerhtml inside perfume div

--- src/components/perfume.js
+++ src/components/perfume.js
@@ -12,12 +12,24 @@ class Perfume{
    }
 
    renderPerfume(){
-       const perfumes = document.querySelector('#perfumes');
+      const perfumes = document.querySelector('#perfumes');
 
-    //    create div
+    // create div
        const perfumeDiv = document.createElement('div');
 
+    // manipulate div
+       perfumeDiv.innnerHTML = `
+         <h3> Name: ${this.name}</h3>
+         <h5> Brand: ${this.brand}</h5> 
+         <h5> Image: ${this.image}</h5>
+       `
 
+    // insert div
+    
+
+
+
+    
    }
 
 

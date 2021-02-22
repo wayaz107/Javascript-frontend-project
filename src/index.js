7108c2edb2a302ca96c6b0acb9fdb7d3cdfc66ca
Wardayaz <wayaz107@gmail.com>
2021-02-22 15:56:12 -0600

    added app instance

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-let seasonAdapter = new SeasonsAdapter("http://localhost:3000/seasons")
-seasonAdapter.getSeasons()
+// let seasonAdapter = new SeasonsAdapter("http://localhost:3000/seasons")
+// seasonAdapter.getSeasons()
 
 
-
+const app = new App
+console.log(app)

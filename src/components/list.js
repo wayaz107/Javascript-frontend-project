5c50a535adc8eb76533036664ba03a1fd0134252
Wardayaz <wayaz107@gmail.com>
2021-02-22 19:54:33 -0600

    added parameters for season

--- src/components/list.js
+++ src/components/list.js
@@ -1,6 +1,9 @@
 class List {
-    constructor(name, description){
+    constructor(name, description, id, season_id, season_name){
         this.name = name
         this.description = description
+        this.id = id
+        this.season_id = season_id
+        this.season_name = season_name
     }
 } 
